c4131bd68f81b37be4b6813f88c3835dc19da0d8
Eduardo <128428496+EJPA0405@users.noreply.github.com>
2025-10-05 23:11:27 -0300

    se crean configuracion y formatos para testing

---
+++ backend/tests/users.spec.js
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "../index.js";
+
+describe("POST /api/users/login and GET /user with token", () => {
+  let token = JWT_SECRET;
+
+  it("responds with json", async () => {
+    const response = await request(app)
+      .post("api/users/login")
+      // utiliza un usuario que exista en la base de datos
+      .send({ email: "correo1@correo.com", password: "123456" });
+
+    token = response.body.token;
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("token");
+    expect(response.body).toHaveProperty("email");
+  });
+
+  //usuario debe tener token para acceder a esta ruta
+  it("responds with json", async () => {
+    const response = await request(app)
+      .get("/api/users")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.results).toBeInstanceOf(Array);
+  });
+});
