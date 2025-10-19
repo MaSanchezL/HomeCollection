b7b0e8d071000ce37d9a18e2e4a06afc66b221a4
Miguel Sánchez <m_alberto89@hotmail.com>
2025-10-19 17:12:12 -0300

    Index.js test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-import app from "./index.js";
+// Sección utilizada para test
 
-const PORT = process.env.PORT || 3000;
+// import app from "./index.js";
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en el puerto ${PORT}`);
+// });
+
